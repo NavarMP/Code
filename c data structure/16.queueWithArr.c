e952fe10a7259176f1d149b98ba10523afb5162d
Muhammed Navar <navarmp@gmail.com>
2024-09-10 11:22:54 +0530

    Update 16.queueWithArr.c

--- c data structure/16.queueWithArr.c
+++ c data structure/16.queueWithArr.c
@@ -1,63 +1,91 @@
-#include<stdio.h>
+#include <stdio.h>
 #include <conio.h>
+#include <stdbool.h>
 
-#define SIZE 10
-void enQueue(int);
+const int QUEUE_SIZE = 10;
+
+int queue[QUEUE_SIZE];
+int front = -1, rear = -1;
+
+void enQueue(int value);
 void deQueue();
 void display();
-int queue[SIZE], front = -1, rear = -1;
-void main()
-{
- int value, choice;
- clrscr();
- while(1){
- printf("\n\n***** MENU *****\n");
- printf("1. Insertion\n2. Deletion\n3. Display\n4. Exit");
- printf("\nEnter your choice: ");
- scanf("%d",&choice);
- switch(choice){
-case 1: printf("Enter the value to be insert: ");
-scanf("%d",&value);
-enQueue(value);
-break;
-case 2: deQueue();
-break;
-case 3: display();
-break;
-case 4: exit(0);
-default: printf("\nWrong selection!!! Try again!!!");
- }
- }
+int getMenuChoice();
+void printMenu();
+
+int main() {
+    int menuChoice;
+    clrscr();
+    while (true) {
+        printMenu();
+        menuChoice = getMenuChoice();
+        switch (menuChoice) {
+            case 1:
+                int insertValue;
+                printf("Enter the value to be inserted: ");
+                scanf("%d", &insertValue);
+                enQueue(insertValue);
+                break;
+            case 2:
+                deQueue();
+                break;
+            case 3:
+                display();
+                break;
+            case 4:
+                exit(0);
+            default:
+                printf("\nWrong selection!!! Try again!!!");
+        }
+    }
+    return 0;
 }
-void enQueue(int value){
- if(rear == SIZE-1)
- printf("\nQueue is Full!!! Insertion is not possible!!!");
- else{
- if(front == -1)
-front = 0;
- rear++;
- queue[rear] = value;
- printf("\nInsertion success!!!");
- }
+
+void enQueue(int value) {
+    if (rear == QUEUE_SIZE - 1) {
+        printf("\nQueue is Full!!! Insertion is not possible!!!");
+    } else {
+        if (front == -1) {
+            front = 0;
+        }
+        rear++;
+        queue[rear] = value;
+        printf("\nInsertion success!!!");
+    }
 }
-void deQueue(){
- if(front == rear)
- printf("\nQueue is Empty!!! Deletion is not possible!!!");
- else{
- printf("\nDeleted : %d", queue[front]);
- front++;
- if(front == rear)
-front = rear = -1;
- }
+
+void deQueue() {
+    if (front == rear) {
+        printf("\nQueue is Empty!!! Deletion is not possible!!!");
+    } else {
+        printf("\nDeleted : %d", queue[front]);
+        front++;
+        if (front == rear) {
+            front = rear = -1;
+        }
+    }
 }
-void display()
-{
- if(rear == -1)
- printf("\nQueue is Empty!!!");
- else{
- int i;
- printf("\nQueue elements are:\n");
- for(i=front; i<=rear; i++)
- printf("%d\t",queue[i]);
- }
+
+void display() {
+    if (rear == -1) {
+        printf("\nQueue is Empty!!!");
+    } else {
+        int i;
+        printf("\nQueue elements are:\n");
+        for (i = front; i <= rear; i++) {
+            printf("%d\t", queue[i]);
+        }
+    }
+}
+
+int getMenuChoice() {
+    int choice;
+    printf("\nEnter your choice: ");
+    scanf("%d", &choice);
+    return choice;
+}
+
+void printMenu() {
+    printf("\n\n***** MENU *****\n");
+    printf("1. Insertion\n2. Deletion\n3. Display\n4. Exit");
 }
